d83eae2c89dc85899bf028fb909d707d6a80c5d0
MSchmidt951 <m.c.schmidt951@gmail.com>
2023-02-13 19:41:50 +0000

    Complete example code

--- src/cube/cube.ino
+++ src/cube/cube.ino
@@ -3,6 +3,8 @@
 #include "Transform.h"
 
 #include "Anim_Rainbow.h"
+#include "Letters.h"
+#include "Anim_Basic.h"
 
 /* How to run an animation
  * 
@@ -23,7 +25,24 @@ void setup(){
   cube.testLights();
   delay(4000);
 
-  cube.startAnim(new Wave(&cube));
+  TypeLetter *hiAnim = new TypeLetter(&cube);
+  hiAnim->addLetter(letters_5.H);
+  hiAnim->addLetter(letters_5.I);
+  hiAnim->addLetter(letters_5.ex);
+  cube.queueAnim(hiAnim);
+
+  cube.queueAnim(new Wave(&cube));
+  Wave *w = new Wave(&cube);
+  w->colour = CRGB::Green;
+  cube.queueAnim(w);
+  Wave *w2 = new Wave(&cube);
+  w2->colour = CRGB::Blue;
+  cube.queueAnim(w2);
+
+  cube.queueAnim(new RainbowRows(&cube));
+  cube.queueAnim(new RainbowSolid(&cube));
+  cube.queueAnim(new RainbowRnd(&cube));
+  cube.queueAnim(new Rain(&cube));
 }
 
 void loop(){
